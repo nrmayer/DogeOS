e75306ccc8cc639cc5dd55e9088fb09e41d09121
nrmayer <noah@the-mayers.net>
2020-04-29 15:51:45 +0000

    its a test

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def loading(msg, t, finish):
     print("|" * i + "-" * f)
     print()
     i = i + 1
-  print(finish)
+  print(finish) #aaaaa
 
 def boot():
   loading('Loading DogeOS...', 0.03, 'DogeOS v0.1 Loaded!')
